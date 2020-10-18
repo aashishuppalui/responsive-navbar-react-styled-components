5d27c90595229848299efe3ff86d2bfdcf058c65
aashishuppal <rogerux09@gmail.com>
2020-10-18 18:59:05 +0530

    responnsive navbar in react using styled components completed

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElement'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to ="/">
+                <img src={require('../../images/logo.svg')} alt='logo' />
+                </NavLink>
+                <Bars/>
+                <NavMenu>
+                    <NavLink to="/about"  activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/services"  activeStyle>
+                        Services
+                    </NavLink>
+                    <NavLink to="/contact"  activeStyle>
+                        Contact
+                    </NavLink>
+                    <NavLink to="/signup"  activeStyle>
+                        Sign Up
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">
+                        Sign In
+                    </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
